c2366c99d2f80ac2895a9b1e2b19b318bb65771f
eclectic_mind <52823445+eclectic-mind@users.noreply.github.com>
2020-09-14 02:39:19 +0300

    feat: 3 functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function minimum(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    }
+    let minimum = array[0];
+    array.forEach((item) => {
+        item < minimum ? minimum = item : minimum;
+    });
+    return minimum;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function maximum(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    }
+    let maximum = array[0];
+    array.forEach((item) => {
+        item > maximum ? maximum = item : maximum;
+    });
+    return maximum;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    }
+    let sum = array.reduce(function (prev, curr) {
+        return prev + curr;
+    });
+    return sum / array.length;
 }
